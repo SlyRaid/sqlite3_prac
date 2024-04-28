c1377bc9f6b22dda078a7ca56ffd36101c9ab28e
Slyr <jezizz@mail.ru>
2024-04-28 20:46:19 +0500

    code

--- main.py
+++ main.py
@@ -21,34 +21,33 @@ class University:
 
     def __init__(self, name):
         self.name = name
+        self.con = sqlite3.connect('students.db')
+        self.cur = self.con.cursor()
 
-    @staticmethod
-    def add_student(name, age):
-        with sqlite3.connect('students.db') as connect:
-            connect.execute("PRAGMA foreign_key = ON")
-            connect.execute(""f"INSERT INTO students (name, age) VALUES ('{name}', '{age}')""")
-            connect.commit()
+    def add_student(self, name, age):
+        with self.con:
+            self.cur.execute("PRAGMA foreign_key = ON")
+            self.cur.execute(""f"INSERT INTO students (name, age) VALUES ('{name}', '{age}')""")
+            self.con.commit()
 
-    @staticmethod
-    def add_grade(student_id, subject, grade):
-        with sqlite3.connect('students.db') as connect:
-            connect.execute("PRAGMA foreign_key = ON")
-            connect.execute(
+    def add_grade(self, student_id, subject, grade):
+        with self.con:
+            self.cur.execute("PRAGMA foreign_key = ON")
+            self.cur.execute(
                 ""f"INSERT INTO grades (student_id, subject, grade) VALUES ('{student_id}', '{subject}', '{grade}')""")
-            connect.commit()
+            self.con.commit()
 
-    @staticmethod
-    def get_students(subject=None):
-        with sqlite3.connect('students.db') as connect:
+    def get_students(self, subject=None):
+        with self.con:
             if subject is None:
-                cursor = connect.execute(
+                cursor = self.cur.execute(
                     ""f"SELECT students.name, students.age, subject, grade FROM students JOIN grades ON "
                     f"students.id = grades.student_id""")
             else:
-                cursor = connect.execute(
+                cursor = self.cur.execute(
                     f"SELECT students.name, students.age, subject, grade FROM students JOIN grades ON "
                     f"students.id = grades.student_id WHERE subject = '{subject}'")
-            connect.commit()
+            self.con.commit()
             return cursor.fetchall()
 
 
@@ -59,15 +58,15 @@ with sqlite3.connect('students.db') as connect:
 
 u1 = University('Urban')
 
-u1.add_student('Ivan', 26)  # id - 1
-u1.add_student('Ilya', 24)  # id - 2
-u1.add_student('Raze', 28)  # id - 3
-u1.add_student('Sova', 33)  # id - 4
-
-u1.add_grade(1, 'Python', 4.8)
-u1.add_grade(2, 'PHP', 4.3)
-u1.add_grade(3, 'Python', 4.5)
-u1.add_grade(4, 'Java', 3.1)
+# u1.add_student('Ivan', 26)  # id - 1
+# u1.add_student('Ilya', 24)  # id - 2
+# u1.add_student('Raze', 28)  # id - 3
+# u1.add_student('Sova', 33)  # id - 4
+#
+# u1.add_grade(1, 'Python', 4.8)
+# u1.add_grade(2, 'PHP', 4.3)
+# u1.add_grade(3, 'Python', 4.5)
+# u1.add_grade(4, 'Java', 3.1)
 
 print(u1.get_students())
 print(u1.get_students('Python'))
